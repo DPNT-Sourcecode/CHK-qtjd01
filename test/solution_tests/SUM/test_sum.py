1254a49fbf3dae5fb95073eae7c056df7577ba66
sbx_user1051 <sbx_user1051@169.254.17.55>
2025-02-28 14:33:11 +0000

    Fri Feb 28 14:33:11 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,23 @@
 from solutions.SUM import sum_solution
+import pytest
+
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_out_of_range(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(101, 2)
+
+    def test_sum_input_not_int(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(1, "2")
+
+    def test_sum_input_float(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(1, 2.0)
+
+
+
