84a2f7419acea390030ef938063447d1e694e119
sbx_user1051 <sbx_user1051@169.254.17.55>
2025-02-28 14:39:09 +0000

    Fri Feb 28 14:39:09 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -2,26 +2,24 @@ from solutions.SUM import sum_solution
 import pytest
 
 
-
 class TestSum():
     def test_sum(self):
         assert sum_solution.sum_solution(1, 2) == 3
 
-    def test_sum_out_of_range(self, capsys):
-        with pytest.raises(ValueError):
-                sum_solution.sum_solution(101, 2)
-                assert "Input out of range" in capsys.readouterr().err
+    def test_sum_out_of_range(self):
+        with pytest.raises(ValueError) as excinfo:
+            sum_solution.sum_solution(101, 2)
+        assert "Input out of range" in str(excinfo.value)
 
-    def test_sum_input_not_int(self, capsys):
-        with pytest.raises(ValueError):
-                sum_solution.sum_solution(1, "2")
-                assert "Input not an integer" in capsys.readouterr().err
+    def test_sum_input_not_int(self):
+        with pytest.raises(ValueError) as excinfo:
+            sum_solution.sum_solution(1, "2")
+        assert "Input not an integer" in str(excinfo.value)
 
-    def test_sum_input_float(self, capsys):
-        with pytest.raises(ValueError):
+    def test_sum_input_float(self):
+        with pytest.raises(ValueError) as excinfo:
             sum_solution.sum_solution(1, 2.0)
-            assert "Input not an integer" in capsys.readouterr().err
-
+        assert "Input not an integer" in str(excinfo.value)
 
 
 
