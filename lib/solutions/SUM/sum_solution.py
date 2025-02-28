fdc2472c40ca71dbd28f3d25c70be0c859760dad
sbx_user1051 <sbx_user1051@169.254.17.55>
2025-02-28 14:30:09 +0000

    Fri Feb 28 14:30:09 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def sum_solution(x: int, y: int) -> int:
+    if x or y not in range[0, 100]:
+        raise ValueError("Input out of range")
+    return x + y
