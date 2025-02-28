f5567ccd4b67e617388e211506e70623919914d3
sbx_user1051 <sbx_user1051@169.254.17.55>
2025-02-28 14:36:09 +0000

    Fri Feb 28 14:36:09 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,19 +5,23 @@ import pytest
 
 class TestSum():
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.sum_solution(1, 2) == 3
 
-    def test_sum_out_of_range(self):
+    def test_sum_out_of_range(self, capsys):
         with pytest.raises(ValueError):
-            sum_solution.compute(101, 2)
+                sum_solution.sum_solution(101, 2)
+                assert "Input out of range" in capsys.readouterr().err
 
-    def test_sum_input_not_int(self):
+    def test_sum_input_not_int(self, capsys):
         with pytest.raises(ValueError):
-            sum_solution.compute(1, "2")
+                sum_solution.sum_solution(1, "2")
+                assert "Input not an integer" in capsys.readouterr().err
 
-    def test_sum_input_float(self):
+    def test_sum_input_float(self, capsys):
         with pytest.raises(ValueError):
-            sum_solution.compute(1, 2.0)
+            sum_solution.sum_solution(1, 2.0)
+            assert "Input not an integer" in capsys.readouterr().err
+
 
 
 
