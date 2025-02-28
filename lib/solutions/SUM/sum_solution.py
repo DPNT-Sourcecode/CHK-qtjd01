21d715f33a64c13f9ce6d2395f44cb4150c928c7
sbx_user1051 <sbx_user1051@169.254.17.55>
2025-02-28 14:42:09 +0000

    Fri Feb 28 14:42:09 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,7 +2,8 @@
 def sum_solution(x: int, y: int) -> int:
     if type(x) is not int or type(y) is not int:
         raise ValueError("Input not an integer")
-    if x or y not in range[0, 100]:
+    if x not in range(0, 100) or y not in range(0, 100):
         raise ValueError("Input out of range")
     return x + y
+
 
